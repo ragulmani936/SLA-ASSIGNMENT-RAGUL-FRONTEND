60f75bb7587f00c942351729d60e51b40c632bde
RAGUL M <mragul168@gmail.com>
2025-12-04 21:56:13 +0530

    update

---
+++ JAVASCRIPT/DAY 1/intro.js
@@ -0,0 +1,16 @@
+/* 1.What does JS do?
+change html Element, change css Style, respond to button clicks, validate forms, create animations, build full apps(React, Node.js, etc..)
+
+
+2.where JS runs?
+browers = chrome, edge, firefoxes, safari Suite, brave, onion tor.
+
+
+JS Engine:
+chrome - v8 engine; firefox - spidermonkey; safari - javascript core; these engines are read JS code line by line
+
+
+*/
+
+//  console.log('ragul')
+
